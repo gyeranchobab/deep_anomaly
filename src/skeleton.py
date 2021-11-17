48134beb74a2fab21000b7d15161fe106e9aff37
gyeranchobab <randrandrre@kaist.ac.kr>
2021-11-17 10:17:02 +0900

    report pickle item

--- src/skeleton.py
+++ src/skeleton.py
@@ -69,8 +69,12 @@ class BaseModel(nn.Module):
             
         if loss is None:
             loss = 0.0
+        elif isinstance(loss, torch.Tensor):
+            loss = loss.item()
         if score is None:
             score = 0.0
+        elif isinstance(score, torch.Tensor):
+            score = score.item()
         
         return loss, score
             
@@ -93,8 +97,8 @@ class BaseModel(nn.Module):
             os.mkdir('./train_logs')
 
         print('(%.2fs) [Epoch %d]'%(time.time()-ts, self.e+1))
-        print('\tTrain Loss : %.4g\tTrain Score : %.4g'%(train_loss, train_score))
-        print('\tEval Loss : %.4g\tEval Score : %.4g'%(eval_loss, eval_score))
+        print('\tTrain Loss : %.5g\tTrain Score : %.5g'%(train_loss, train_score))
+        print('\tEval Loss : %.5g\tEval Score : %.5g'%(eval_loss, eval_score))
         torch.save(self.state_dict(), './saved_models/%s_e%d.pth'%(self.model_name, self.e+1))
         with open('./train_logs/%s.bin'%(self.model_name), 'wb') as f_log:
             pickle.dump(self.logs, f_log)
