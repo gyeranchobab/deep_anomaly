01ee7aa83d903d6b8c839ebbd32e368f1abed1a0
KanghoonYoon <ykhoon0827@gmail.com>
2021-11-18 22:15:05 +0900

    requirements

---
+++ install.sh
@@ -0,0 +1,8 @@
+conda install h5py --yes
+conda install numpy --yes
+conda install matplotlib --yes
+conda install scikit-learn --yes
+conda install scipy --yes
+conda install jupyter --yes
+conda install tqdm --yes
+conda install pytorch==1.8.0 torchvision==0.9.0 torchaudio==0.8.0 cudatoolkit=10.2 -c pytorch --yes
